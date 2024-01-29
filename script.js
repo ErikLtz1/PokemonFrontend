9e657e72bd339de07de5f3ca6aefc81a642b0ddf
ErikLtz1 <eriklilliecreutz@gmail.com>
2024-01-29 15:37:38 +0100

    Får ut pokenamn i ul (Fix #9)

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-fetch("https://pokeapi.co/api/v2/pokemon")
-.then(res => res.json())
-.then(data => {
-    console.log(data);
-})
+let pokemonList = document.getElementById("pokemonList");
+
+fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+  .then(res => res.json())
+  .then(data20 => {
+    data20.results.forEach(pokemon => {
+      let li = document.createElement("li");
+      let link = document.createElement("a");
+
+      link.href = pokemon.url;
+      link.innerText = pokemon.name;
+
+      li.appendChild(link);
+      pokemonList.appendChild(li);
+    });
+  })
+  .catch(error => {
+    console.error("Något gick fel:", error);
+  });
+
 
