da3e774270b4a700fc13b58691d3fa34ef311ad5
ErikLtz1 <eriklilliecreutz@gmail.com>
2024-01-31 22:04:29 +0100

    Backup kommit en bit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let pokemonList = document.getElementById("pokemonList");
 let pokeImg = document.getElementById("pokeImg");
+let pokedexBtn = document.getElementById("pokedexBtn");
+
+let pokemonCaptured = false;
 
 fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
   .then(res => res.json())
   .then(data20 => {
-    
     data20.results.forEach(pokemon => {
       let li = document.createElement("li");
       let link = document.createElement("a");
@@ -13,7 +15,7 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
       link.innerText = pokemon.name;
 
       link.addEventListener("click", function (event) {
-        event.preventDefault(); 
+        event.preventDefault();
         fetchPokemonDetails(pokemon.url);
       });
 
@@ -26,11 +28,9 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
   });
 
 function fetchPokemonDetails(url) {
-  
   fetch(url)
     .then(res => res.json())
     .then(pokemonDetails => {
-      
       let imageUrl = pokemonDetails.sprites.front_default;
 
       let img = document.createElement("img");
@@ -38,13 +38,37 @@ function fetchPokemonDetails(url) {
 
       let catchButton = document.createElement("button");
       catchButton.innerText = "Fånga Pokemon";
+
       catchButton.addEventListener("click", function (event) {
-        event.preventDefault(); 
+        event.preventDefault();
         console.log("Fångade Pokemon: " + pokemonDetails.name);
+        alert("En ny Pokémon har lagts till i din Pokedex!");
+
+        let existingPokemon = {
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+        };
+
+        fetch("http://localhost:8080/pokemon", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(existingPokemon)
+        })
+        .then(res => res.json())
+        .then(response => {
+          console.log(response);
+          pokemonCaptured = true;
+          
+          
+        })
+        .catch(error => {
+          console.error("Något gick fel vid skapande av Pokemon:", error);
+        });
       });
 
       pokeImg.innerHTML = "";
-
       pokeImg.appendChild(img);
       pokeImg.appendChild(catchButton);
 
@@ -55,3 +79,14 @@ function fetchPokemonDetails(url) {
       console.error("Något gick fel vid hämtning av Pokemon-detaljer:", error);
     });
 }
+
+pokedexBtn.addEventListener("click", function (event) {
+      fetch("http://localhost:8080/pokemons")
+        .then(res => res.json())
+        .then(pokedex => {
+      event.preventDefault();
+      console.log(pokedex);
+    });
+  });
+
+
